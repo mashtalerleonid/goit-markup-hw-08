ab5ca50eed2be03fa189c768461190c16e5048aa
mashtalerleonid <mashtalerleonid@gmail.com>
2021-04-04 22:17:24 +0300

    mobile menu fix

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,22 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
 
-    menuBtnRef.addEventListener("click", () => {
-        const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-        menuBtnRef.classList.toggle("menu--is-open");
-        menuBtnRef.setAttribute("aria-expanded", !expanded);
-        mobileMenuRef.classList.toggle("menu--is-open");
+  const cont = document.querySelector('[data-link]');
 
-        document.body.classList.toggle("menu--is-open")
+  menuBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+    menuBtnRef.classList.toggle('menu--is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+    mobileMenuRef.classList.toggle('menu--is-open');
 
-    });
-})()
+    document.body.classList.toggle('menu--is-open');
+  });
+
+  cont.addEventListener('click', () => {
+    document.body.classList.remove('menu--is-open');
+    mobileMenuRef.classList.remove('menu--is-open');
+    menuBtnRef.classList.remove('menu--is-open');
+  });
+})();
